9693cef3e7ef42d549856e37f52da988c52fd72c
Gabby Anthony <anthony.373@osu.edu>
2024-10-08 19:39:10 -0400

    Minor changes to payment

--- frontend/src/Payment/Payment.tsx
+++ frontend/src/Payment/Payment.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import CheckoutSteps from './CheckoutSteps';
+import CheckoutSteps from '../Payment/CheckoutSteps';
 
 function PaymentScreen(){
     const submitHandler = () => {
