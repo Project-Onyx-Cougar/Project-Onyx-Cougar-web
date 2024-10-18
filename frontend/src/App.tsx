7b48931a39d061ee60248edb92e6d74cd8843b68
Praveen Kumar <p_kumar2@yahoo.com>
2024-10-18 16:31:02 -0400

    Fixed App.tsx and updated react-router-dom version

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   const openMenu = () => {
     document.querySelector(".sidebar")?.classList.add("open");
   }
-  
+
   const closeMenu = () => {
     document.querySelector(".sidebar")?.classList.remove("open");
   }
@@ -21,52 +21,46 @@ function App() {
   return (
     <Router>
       <div className="grid-container">
-      <head>
-        <link rel="stylesheet" href="style.css" />
-        <title>Onyx Cougar</title>
-
-        <body>
-          <div>
-            <header className="header">
-              <div className="brand">
-                  <button onClick={openMenu}>&#9776;</button>
-                  <a href="index.html">Onyx Cougar</a>
-              </div>
-              <div className="header-links">
-                  <Link to="/">Home</Link>
-                  <Link to="/catalog">Catalog</Link>
-                  <Link to="/orders">Orders</Link>
-                  <Link to="/payment">Payment</Link>
-              </div>
-            </header>
-            <aside className="sidebar">
-              <h3>Shopping Categories</h3>
-              <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-              <ul>
-                  <li>
-                      <a href="index.html">Pants</a>
-                  </li>
-
-                  <li>
-                      <a href="index.html">Shirts</a>
-                  </li>
-              </ul>
-            </aside>
-            <main className="main">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/catalog" element={<Products />} />
-                <Route path="orders" element={<Orders />}></Route>
-                <Route path="/order/:id" element={<OrderDetail />}></Route>
-                <Route path="/payment" element ={<CheckoutSteps />}></Route>
-              </Routes>
-            </main>
-            <footer className= "footer">
-              &copy; 2022 Onyx Cougar
-            </footer>
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <a href="index.html">Onyx Cougar</a>
           </div>
-        </body>
-      </head>
+          <div className="header-links">
+            <Link to="/">Home</Link>
+            <Link to="/catalog">Catalog</Link>
+            <Link to="/orders">Orders</Link>
+            <Link to="/payment">Payment</Link>
+          </div>
+        </header>
+
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+          <ul>
+            <li>
+              <Link to="/">Pants</Link>
+            </li>
+
+            <li>
+              <Link to="/">Shirts</Link>
+            </li>
+          </ul>
+        </aside>
+
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalog" element={<Products />} />
+            <Route path="orders" element={<Orders />}></Route>
+            <Route path="/order/:id" element={<OrderDetail />}></Route>
+            <Route path="/payment" element={<CheckoutSteps />}></Route>
+          </Routes>
+        </main>
+
+        <footer className="footer">
+          &copy; 2022 Onyx Cougar
+        </footer>
       </div>
     </Router>
   );
